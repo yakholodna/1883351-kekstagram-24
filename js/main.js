0076ac0d2c61abff74455357938d2fff348893f5
yakholodna <91069675+yakholodna@users.noreply.github.com>
2021-09-27 16:39:55 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
 function getRandomInt(from, to) {
   return Math.floor(Math.random()*(to - from + 1) + from);
 }
-
 getRandomInt(2, 10);
-
 function checkLengthRequirement(string, maxLength) {
   if (string.length <= maxLength) {
     return true;
   }
-  
   return false;
 }
-
 checkLengthRequirement('hello, world!', 5);
