1e0e9bcce7acc63d68a4cf2a75737fdf0ea8ec7d
liza <liza.kholodna@gmail.com>
2021-10-05 15:45:08 +0300

    module4-task1-try2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const getRandomInt = (from, to) => Math.floor(Math.random()*(to - from + 1) + from);
+const getRandomInt = (from, to) => Math.floor(Math.random() * (to - from + 1) + from);
 getRandomInt(2, 10);
 const checkLengthRequirement = (string, maxLength) => string.length <= maxLength;
 checkLengthRequirement('hello, world!', 5);
@@ -28,42 +28,41 @@ const generateComment = () => {
   const randomCommentInt = getRandomInt(0, COMMENT_SUGGESTIONS.length - 1);
   const randomCommentAuthorInt = getRandomInt(0, COMMENT_AUTHORS.length - 1);
   return {
-    id : getRandomInt(0, 1000),
-    avatar : `img/avatar-${  getRandomInt(1,6)  }.svg`,
-    message : COMMENT_SUGGESTIONS[randomCommentInt],
-    name : COMMENT_AUTHORS[randomCommentAuthorInt],
-  }
+    id: getRandomInt(0, 1000),
+    avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
+    message: COMMENT_SUGGESTIONS[randomCommentInt],
+    name: COMMENT_AUTHORS[randomCommentAuthorInt],
+  };
 };
 
 // Creates one picture object
 const createPicture = (idNum, allComments) => {
   return {
-    id : idNum,
-    url : 'photos/' + idNum + '.jpg',
-    description : 'description of picture goes here',
-    likes : getRandomInt(15, 200),
-    comments : allComments,
-  }
-};
+    id: idNum,
+    url: 'photos/' + idNum + '.jpg',
+    description: 'description of picture goes here',
+    likes: getRandomInt(15, 200),
+    comments: allComments,
+  };
+}
 
 // Creates an array of pictures
 const generatePictures = () => {
   // Array of 25 pictures
   const pictures = [];
-  // For loop for creating the array
-  for (let i = 1; i < 26; i++) {
+  // For loop for creating the array of pictures
+  for (let pic = 1; pic < 26; pic++) {
     // Array of comments for each picture
     const comments = [];
     // For loop that creates 5 comments
-    for (let j = 0; j < 5; j++) {
+    for (let com = 0; com < 5; com++) {
       comments.push(generateComment());
     }
     // Adds newly created picture w/ unique id and comments to array of pictures
-    pictures.push(createPicture(i, comments));
-    console.log(comments);
+    pictures.push(createPicture(pic, comments));
   }
   return pictures;
-};
-console.log(generatePictures());
+}
+generatePictures();
 
 
