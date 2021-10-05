6f636470df535090435fbaec536064801a38a1bd
liza <liza.kholodna@gmail.com>
2021-10-05 19:05:17 +0300

    module4-task1-try8

--- js/main.js
+++ js/main.js
@@ -23,6 +23,10 @@ const COMMENT_AUTHORS = [
   'Мая',
 ];
 
+// Constants
+const numOfPics = 26;
+const numOfComments = 5;
+
 // Creates one comment object
 const generateComment = () => {
   const randomCommentInt = getRandomInt(0, COMMENT_SUGGESTIONS.length - 1);
@@ -49,11 +53,11 @@ const generatePictures = () => {
   // Array of 25 pictures
   const pictures = [];
   // For loop for creating the array of pictures
-  for (let pic = 1; pic < 26; pic++) {
+  for (let pic = 1; pic < numOfPics; pic++) {
     // Array of comments for each picture
     const comments = [];
     // For loop that creates 5 comments
-    for (let com = 0; com < 5; com++) {
+    for (let com = 0; com < numOfComments; com++) {
       comments.push(generateComment());
     }
     // Adds newly created picture w/ unique id and comments to array of pictures
