927b9f7429c371fa059ac911608a00e2a3c9b506
liza <liza.kholodna@gmail.com>
2021-11-17 20:09:46 +0300

    module12-task2-try2

--- js/user-picture.js
+++ js/user-picture.js
@@ -5,9 +5,7 @@ const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 fileUpload.addEventListener('change', () => {
   const file = fileUpload.files[0];
   const fileName = file.name.toLowerCase();
-  const matches = FILE_TYPES.some((it) => {
-    return fileName.endsWith(it);
-  });
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
   if (matches) {
     preview.src = URL.createObjectURL(file);
   }
